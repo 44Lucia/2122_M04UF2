60c387afec435a647c93f73efd786d292bd28fad
enti <enti@enti.enti>
2022-03-31 18:37:49 +0200

    Añadir(+) y Ventanamodal

--- todolist/src/RemoveTask.js
+++ todolist/src/RemoveTask.js
@@ -1,15 +1,72 @@
+import React from 'react';
+
 import Button from '@mui/material/Button';
-
 import DeleteIcon from '@mui/icons-material/Delete';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
 
-function RemoveTask (props)
-{
-	return (
-		<Button startIcon={<DeleteIcon />} onClick={function (event){
-			props.removeTask(props.id_task);
-    	}
-		}>BORRAR</Button>
+import DialogTitle from '@mui/material/DialogTitle';
+
+class RemoveTask extends React.Component{
+
+	constructor (props){
+		super(props);
+		this.state = { 
+			open: false
+		 };
+	}
+
+	handleOnClick = ()=>{
+		this.setState({open: true });
+	}
+
+
+	handleClose = () => {
+		this.setState({open:false});
+    }
+
+	handleDelete = () =>{
+		this.props.removeTask(this.props.task_id);
+		this.setState({open:false});
+	}
+
+
+ 	render() {
+ 		return (
+		<div>
+			<Button variant="contained" 
+					startIcon=<DeleteIcon /> 
+					onClick={this.handleOnClick}			
+				
+			>	
+
+			BORRAR
+		 
+			</Button>
+
+			<Dialog  
+				open={this.state.open}
+				onClose={this.handleClose}
+				>
+				<DialogTitle>
+					{'Estas seguro de que quieres borrarlo?'}
+				</DialogTitle>
+
+				<DialogActions>
+					<Button onClick={this.handleDelete}>
+						BORRAR
+					</Button>
+				<Button onClick={this.handleClose}> 
+					Salir
+				</Button>
+
+			</DialogActions>
+		</Dialog>
+	</div>
+
 	);
-}
 
+	}
+}
 export default RemoveTask;
--- todolist/src/TaskSubmit.js
+++ todolist/src/TaskSubmit.js
@@ -1,9 +1,24 @@
-function TaskSubmit ()
-{
-	return (
-		<button>Añadir</button>
-	);
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
 
+import React from 'react';
+
+class  TaskSubmit extends React.Component{
+	
+	constructor (props){
+		super(props);
+		this.state ={
+
+		}
+	}
+
+	render(){
+		return(
+			<Button startIcon={<AddIcon onClick=
+			{this.props.HandleOnSubmit} />}></
+			Button>
+		);
+	}
 }
 
 export default TaskSubmit;
